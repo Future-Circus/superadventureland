0f070b57aec354fe103af3fbff40253be40195f0
Aidan <aidan@dreampark.app>
2025-10-28 16:58:43 -0700

    palm tree now rustles! sfx

--- Assets/Content/SuperAdventureLand/Scripts/AudioController.cs
+++ Assets/Content/SuperAdventureLand/Scripts/AudioController.cs
@@ -1,5 +1,6 @@
 ﻿namespace SuperAdventureLand
 {
+    using System;
     using UnityEngine;
 
     public enum AudioControl {
@@ -14,12 +15,18 @@
         private Vector3 previousPosition;
         public AudioControl audioControl = AudioControl.NONE;
 
+        private float lastValue = 0f;
+
+        public void Start() {
+            previousPosition = transform.position;
+        }
         public void Update()
         {
             float velocity = (transform.position - previousPosition).magnitude;
             if (audioControl == AudioControl.VELOCITY) {
                 audioSource.pitch = 1f + (velocity / 10f);
-                audioSource.volume = Mathf.Clamp(velocity * sensitivity, 0f, 1f);
+                audioSource.volume = Mathf.Clamp(Mathf.Lerp(lastValue, velocity * sensitivity, Time.deltaTime * 10f), 0f, 1f);
+                lastValue = audioSource.volume;
             }
             previousPosition = transform.position;
         }
