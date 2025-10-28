3ae6ba8831c58228b8cce5b3cb98a58cd2f1465f
Aidan <aidan@dreampark.app>
2025-10-28 16:59:08 -0700

    ++EasySpawn (for SuperAdventureLand)

---
+++ Assets/Content/SuperAdventureLand/5. EasyEvent/EasySpawn.cs
@@ -0,0 +1,30 @@
+﻿namespace SuperAdventureLand
+{
+    #if UNITY_EDITOR
+    using UnityEditor;
+    #endif
+    using UnityEngine;
+
+    public class EasySpawn : DreamPark.Easy.EasySpawn
+    {
+
+    #if UNITY_EDITOR
+        public void OnValidate() {
+            if (spawnPrefab == null) {
+                spawnPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Content/SuperAdventureLand/Prefabs/E_COIN.prefab");
+            }
+        }
+    #endif
+        public override void OnEvent(object arg0 = null) {
+            if (spawnPrefab.layer == LayerMask.NameToLayer("Item")) {
+                GameObject item = Spawn(spawnPrefab, spawnPoint);
+                if (item.TryGetComponent(out Item itemScript)) {
+                    itemScript.PopUpItem(new Vector3(Random.Range(-0.1f, 1f), 2, Random.Range(-0.1f, 1f)));
+                }
+                onEvent?.Invoke(null);
+            } else {
+                base.OnEvent(arg0);
+            }
+        }
+    }
+}
