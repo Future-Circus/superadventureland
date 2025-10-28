7d484938f63e4064b5623952ccac7e93d1303124
Aidan <aidan@dreampark.app>
2025-10-27 22:49:27 -0700

    ++EasyThrow Super Adventure boilerplate

---
+++ Assets/Content/SuperAdventureLand/5. EasyEvent/EasyThrow.cs
@@ -0,0 +1,23 @@
+﻿namespace SuperAdventureLand {
+using System.Linq;
+using UnityEngine;
+    public class EasyThrow : DreamPark.Easy.EasyThrow
+    {
+        public void OnValidate() {
+            if (targetLayerOrder == null || targetLayerOrder.Length == 0) {
+                targetLayerOrder = new string[] { "Entity", "Level" };
+            }
+        }
+        public override void OnEvent(object arg0 = null)
+        {
+            CollisionWrapper lastCollision = arg0 as CollisionWrapper;
+            if (lastCollision != null && lastCollision.gameObject != null && !lastCollision.gameObject.IsDestroyed() && lastCollision.gameObject.TryGetComponent(out PlayerInteractor interactor)) {
+                Throw(interactor.GetDirection(), interactor.GetVelocity());
+                onEvent?.Invoke(null);
+            } else {
+                base.OnEvent(arg0);
+            }
+        }
+    }
+
+}
